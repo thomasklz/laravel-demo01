268f06ce6c0245c819ec5cb6304c71922d328b25
thomasklz <pipos_rgt@hotmail.com>
2020-12-16 09:52:19 -0500

    proyecto basico

---
+++ resources/views/registro.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Registro</h1>
+	<form action="#" method="get" >
+		
+		<div>
+			<label> user
+				<input type="text" name="user" placeholder="ingrese su nombre">
+			</label>
+		</div>
+
+		<div>
+			<label> contraseña
+				<input type="password" name="pass" placeholder="ingrese su contraseña">
+			</label>
+		</div>
+	</form>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+<h1>Tomás Loor</h1>
